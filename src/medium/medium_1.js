b0163c77d6cb93f0a4a254d6c297344c1d77f8cc
Diksha Joshi <dikshaj679@gmail.com>
2021-11-23 20:42:06 -0500

    did medium1

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,7 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-    let sum; 
+    let sum = 0; 
     for (let i=0; i<array.length; i++) {
         sum += array[i];
     }
@@ -26,7 +26,12 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-
+    array.sort();
+    mid = array[Math.floor(array.length/2)];
+    if (array.length%2 == 0) {
+        mid = ((mid + array[Math.floor(array.length/2 - 1)])/2);
+    }
+    return mid;
 }
 
 /**
@@ -49,6 +54,16 @@ export function getMedian(array) {
  }
  */
 export function getStatistics(array) {
-
+    let stats = {
+        length: array.length, 
+        sum: getSum(array), 
+        mean: getSum(array)/array.length, 
+        median: getMedian(array), 
+        min: Math.min.apply(null, array), 
+        max: Math.max.apply(null, array), 
+        variance: variance(array, getSum(array)/array.length), 
+        standard_deviation: Math.sqrt(variance(array, getSum(array)/array.length))
+    };
+    return stats;
 }
 
