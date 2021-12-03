6fc47bdaee552e88894d2409041e3021138ff78e
Diksha Joshi <dikshaj679@gmail.com>
2021-12-02 21:44:45 -0500

    spicy

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -44,7 +44,7 @@ export const multiplyBy = (num1) => {
     const retMult = (num2) => {
         return num1*num2;
     }
-    return retMult();
+    return retMult;
 };
 
 
@@ -52,8 +52,8 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = (ten) => {
-    
+export const tenTimes = (num) => {
+    return multiplyBy(num)(10);
 };
 
 
@@ -62,7 +62,7 @@ export const tenTimes = (ten) => {
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
@@ -90,7 +90,7 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-    for (let i=0; i<arr.length; i=i+2) {
+    for (let i=0; i<arr.length; i+=2) {
         if (!test(arr[i])) {
             return false;
         }
@@ -119,7 +119,7 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-    for (let i=0; i<arr.length; i=i+2) {
+    for (let i=0; i<arr.length; i+=2) {
         if (test(arr[i])) {
             return true;
         }
@@ -179,7 +179,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    return someEven(arr, x => {x%2 != 0});
+    return someEven(arr, x => x%2 != 0);
 };
 
 
@@ -190,9 +190,5 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-    let res = filter(arr, test);
-    if (res.pass.length == n) {
-        return true;
-    }
-    return false;
+    return filter(arr,test).pass.length == n;
 };
